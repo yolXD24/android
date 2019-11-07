d00ed06ebdc8885a923611c8c3a1277621ff92c5
tanilonch_sd2023@ceaccs.net <tanilonch_sd2023@ceaccs.net>
2019-11-07 18:46:23 -0500

    Android

--- MainActivity.java
+++ MainActivity.java
@@ -7,16 +7,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
     Button login = null;
-    EditText username = null;
-    EditText password = null;
+    EditText username, password;
     Button register = null;
     Button create = null;
-    Button back = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,13 +35,13 @@ public class MainActivity extends AppCompatActivity {
         login = (Button) findViewById(R.id.btn_login);
         create = (Button) findViewById(R.id.btn_registerPage);
         register = (Button) findViewById(R.id.btn_register);
-        username = (EditText) findViewById(R.id.text_username);
-        password = (EditText) findViewById(R.id.text_password);
-        back = (Button) findViewById(R.id.btn_back);
+        username = (EditText) findViewById(R.id.text_usernameReg);
+        password = (EditText) findViewById(R.id.text_passwordReg);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), username.getText(), Toast.LENGTH_LONG).show();
+//                Toast.makeText(getApplicationContext(), username.getText(), Toast.LENGTH_LONG).show();
+                volley();
             }
         });
 
@@ -45,8 +52,35 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    
+    public void volley(){
+        final TextView textView = (TextView) findViewById(R.id.resText);
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://172.16.11.22:4000/api/login/"+username.getText().toString()+"/"+password.getText().toString();
+        Toast.makeText(getApplicationContext(), username.getText(), Toast.LENGTH_LONG).show();
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        textView.setText("Response is: "+ response);
 
 
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+        
     }
 
 
--- Register.java
+++ Register.java
@@ -3,12 +3,78 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class Register extends AppCompatActivity {
 
+    Button back = null;
+    EditText username, password;
+    Button register = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        init();
     }
-}
+
+    public void init(){
+        back = (Button) findViewById(R.id.btn_back);
+        register = (Button) findViewById(R.id.btn_register);
+        username = (EditText) findViewById(R.id.text_usernameReg);
+        password = (EditText) findViewById(R.id.text_passwordReg) ;
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                register();
+            }
+        });
+    }
+
+    public void register(){
+
+        final TextView textView = (TextView) findViewById(R.id.resText2);
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://172.16.11.22:4000/api/register/"+username.getText().toString()+"/"+password.getText().toString();
+        Toast.makeText(getApplicationContext(), username.getText().toString(), Toast.LENGTH_LONG).show();
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        textView.setText("Response is: "+ response);
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
+    }
+    }
+
