c6cf7873d150cf82b67ea1307a05f2ebcb5a135c
tanilonch_sd2023@CEACCS.NET <tanilonch_sd2023@CEACCS.NET>
2019-11-11 05:29:34 -0500

    transfer

--- MyApplication/app/src/main/java/com/example/myapplication/Register.java
+++ MyApplication/app/src/main/java/com/example/myapplication/Register.java
@@ -19,7 +19,7 @@ import com.android.volley.toolbox.Volley;
 public class Register extends AppCompatActivity {
 
     Button back = null;
-    EditText username, password;
+    EditText username, password, confirmPassword;
     Button register = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +33,7 @@ public class Register extends AppCompatActivity {
         register = (Button) findViewById(R.id.btn_register);
         username = (EditText) findViewById(R.id.text_usernameReg);
         password = (EditText) findViewById(R.id.text_passwordReg) ;
+        confirmPassword = (EditText) findViewById(R.id.text_confirmPasswordReg);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,32 +44,42 @@ public class Register extends AppCompatActivity {
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                register();
+                if(username.getText().toString().isEmpty()||password.getText().toString().isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Please provide a username and password!", Toast.LENGTH_LONG).show();
+                }else{
+                    if(password.getText().toString().equals(confirmPassword.getText().toString())){
+                        registerVolley(username.getText().toString(), password.getText().toString());
+                    }else{
+                        Toast.makeText(getApplicationContext(), "Password did not match!", Toast.LENGTH_LONG).show();
+                    }
+                }
+
             }
         });
     }
 
-    public void register(){
+    public void registerVolley(String username, String password){
 
-        final TextView textView = (TextView) findViewById(R.id.resText2);
+//        final TextView textView = (TextView) findViewById(R.id.resText2);
 
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="http://172.16.11.22:4000/api/register/"+username.getText().toString()+"/"+password.getText().toString();
-        Toast.makeText(getApplicationContext(), username.getText().toString(), Toast.LENGTH_LONG).show();
+        String url ="http://172.16.11.8:3000/api/register/"+username+"/"+password;
+
         // Request a string response from the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         // Display the first 500 characters of the response string.
-                        textView.setText("Response is: "+ response);
+//                        textView.setText("Response is: "+ response);
+                        Toast.makeText(getApplicationContext(), "Registered successfully", Toast.LENGTH_LONG).show();
 
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                textView.setText("That didn't work!");
+                Toast.makeText(getApplicationContext(), "Cannot connect to the server!\nPlease try again later.", Toast.LENGTH_LONG).show();
             }
         });
 
